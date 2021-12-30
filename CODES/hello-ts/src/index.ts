7e06ab745a92dbcad052c05523e8e475b6f0b5d5
Li-HONGYAO <lihy_online@163.com>
2021-12-30 19:23:27 +0800

    优化更新

--- CODES/hello-ts/src/index.ts
+++ CODES/hello-ts/src/index.ts
@@ -2,20 +2,12 @@
  * @Author: Lee
  * @Date: 2021-12-29 13:21:32
  * @LastEditors: Lee
- * @LastEditTime: 2021-12-29 14:12:05
+ * @LastEditTime: 2021-12-30 18:04:27
  */
 
-// -- 定义接口
-interface IProps {
-  name: string;
-  age: number;
-  job: string;
-}
-// -- 定义一个遍历，实现IProps接口
-const usr: IProps = {
-  name: 'Li-HONGYAO',
-  age: 18,
-  job: '前端工程师',
-};
-// -- 输出变量
-console.log(usr);
+
+
+
+
+
+
